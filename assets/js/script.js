087cfff2d285127e5b733f51db80b24314ca0f81
Bret Banger <Banger484@gmail.com>
2022-04-04 13:10:49 -0500

    starting on cocktail data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,26 @@
 var cocktailByLetter = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
+var container = document.getElementById('container')
+var drink;
 
-function getCocktails(x) {
-  fetch(`${cocktailByLetter}${x}`)
+
+
+
+function getCocktails(firstLetter) {
+  fetch(`${cocktailByLetter}${firstLetter}`)
     .then(function (res) {
       if (res.status !== 200) {
         console.log("fetch found nothing!");
         return;
       }
       res.json().then(function (data) {
-        console.log(data);
+        drink = data.drinks[1]
+        console.log(drink)
+        console.log(`${drink.strMeasure1} ${drink.strIngredient1}`)
+        console.log(`${drink.strMeasure1} ${drink.strIngredient1}`)
+  console.log(`${drink.strMeasure2} ${drink.strIngredient2}`)
+  console.log(`${drink.strMeasure3} ${drink.strIngredient3}`)
+  console.log(`${drink.strMeasure4} ${drink.strIngredient4}`)
+       
       });
     })
     .catch(function (err) {
@@ -17,3 +29,12 @@ function getCocktails(x) {
 }
 
 getCocktails("c");
+
+function getRecipe (d) {
+if ()
+
+
+
+}
+
+getRecipe()
