399cdf6755ed095f8135ae72761ab13f8467aa0f
abelzer09 <aaronrbelzer@gmail.com>
2022-04-06 10:31:13 -0500

    saved

--- assets/js/search.js
+++ assets/js/search.js
@@ -7,8 +7,8 @@ var citySearchVal;
 
 function searchInput(event){
 	event.preventDefault()
-	var userNameVal = document.getElementById('user-input').value
-	var citySearchVal = document.getElementById("city-input").value;
+	userNameVal = document.getElementById('user-input')
+	citySearchVal = document.getElementById("city-input")
 
     if (citySearch.value == 0 || userName.value == 0 ) {
         alert("must enter a vaild user name and city!");
